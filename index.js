d9e6cddc11678f56742c299e80c4e21a3f4339cb
Anis FETOUI <anis.fetoui@esprit.tn>
2023-12-27 00:30:20 +0100

    add request header-parser to api/whoami

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.get('/api/whoami',(req, res) =>{
+  res.json({ ipaddress: req.ip,
+    language:req.headers["accept-language"],
+    software:req.headers["user-agent"]});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
